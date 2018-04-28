e525070765530c53c44467ec14cbf883efb844ed
Šarūnas Ročevas <srocevas@gmail.com>
2018-04-28 18:59:22 +0300

    Remove error handler

--- gulp-tasks/html-render.js
+++ gulp-tasks/html-render.js
@@ -4,21 +4,15 @@ module.exports = function () {
 		panini = require('panini'),
 		config = require('../gulp.config.js')();
 
-	function swallowError(error) {
-		// If you want details of the error in the console
-		console.log('\x1b[36m', error.toString(), '\x1b[0m');
-		this.emit('end');
-	}
-
 	return gulp.src(config.html.src)
 		.pipe(panini({
 			root: 'src/html',
+			partials: 'src/html/components',
 			layouts: 'src/layouts',
 			pageLayouts: {
 				'components': 'components'
 			}
 		}))
 		.pipe(inky())
-		.on('error', swallowError) //.pipe(htmlmin({collapseWhitespace: true,minifyCSS: true}))
 		.pipe(gulp.dest(config.html.dest));
 };
